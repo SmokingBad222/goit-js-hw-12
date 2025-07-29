b86a459af638c6fccad10ec5add21d4093b96607
Pashka Antonov <pavelcreed222@gmail.com>
2025-07-29 20:37:19 +0300

    hw-12

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -33,9 +33,7 @@ export function hideLoader() {
   loader.classList.remove('is-visible');
 }
 
-export function showLoadMoreButton() {
-  console.log("show button");
-  
+export function showLoadMoreButton() {  
   loadMoreBtn.classList.remove('is-hidden');
 }
 
